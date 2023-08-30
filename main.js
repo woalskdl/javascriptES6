cd4b5a72d8bda2470a395c9b2621158dfff3e642
wooljam <woalskdl@gmail.com>
2023-08-30 23:00:03 +0900

    연산자와 구문 진행중

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+
+// 1. JS 데이터
+console.log('=============== JS 데이터 ================')
+
 const string1 = 'hello'
 const string2 = `hello ${string1} ?!`   // 템플릿 리터럴 - 기호
 
@@ -40,3 +44,71 @@ const user2 = new User();
 console.log('user2 >> ', user2)
 console.log(user.name);
 console.log(user['name'])
+
+function hello() {
+    return 'hello!';
+}
+
+console.log(hello) // 함수를 호출하는게 아닌 변수로서 취급
+console.log(hello()) // 함수를 호출
+console.log(typeof hello)   // function
+console.log(typeof hello()) // string
+
+const aFunction = function() {
+    console.log('A');
+}
+
+const bFunction = function(c) {
+    console.log(c)
+    if (typeof c === 'function')
+        c()
+}
+
+bFunction (123)
+bFunction(aFunction)
+
+if (123) {
+    console.log('참!')
+}
+
+if (0) {
+    console.log('참!!') // 안나옴
+    // 1) false
+    // 2) 0
+    // 3) null
+    // 4) undefined
+    // 5) NaN
+    // 6) '' (빈 문자열)
+    // 7) 0n (BigInt)
+}
+
+console.log(typeof undefined === 'undefined')   // true
+console.log(typeof null === 'object')           // true
+console.log(typeof [] === 'object')             // true
+console.log(typeof {} === 'object')             // true
+
+console.log([].constructor === Array)           // Array 라는 javascript의 전역 함수
+console.log({}.constructor === Object)          // Object 라는 javascript의 전역 함수
+// console.log(null.constructor)                   // 문법 오류
+
+console.log(Object.prototype.toString.call(null).slice(8, -1) === 'Null');
+
+function checkType(data) {
+    return Object.prototype.toString.call(data).slice(8, -1);
+}
+
+console.log(checkType(null))        // Null
+console.log(checkType([]));         // Array
+console.log(checkType({}));         // Object
+console.log(checkType(undefined))   // Undefined
+console.log(checkType(() => {}))    // Function
+
+// 2. 연산자와 구문
+console.log('============= 연산자와 구문 ==============')
+
+console.log(!0)         // true
+console.log(!!0)        // false
+console.log(!null)      // true
+console.log(!undefined) // true
+console.log(!NaN)       // true
+console.log(!'')        // true
